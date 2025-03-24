116e902a4ceb746419c19e74a943bb59bcec4b0f
Süleyman Korcan ÜNALAN <103763684+skunalan@users.noreply.github.com>
2025-03-25 02:44:01 +0300

    unused components removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import ReactDom from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes";
 
 createRoot(document.getElementById("root")!).render(
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
 );
